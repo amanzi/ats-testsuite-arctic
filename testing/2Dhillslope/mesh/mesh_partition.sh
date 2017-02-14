600175e75312652552eaf8f16ea30f07d0ef262f
Elchin <elchin.jafarov@gmail.com>
2017-02-14 10:13:01 -0700

    added varying organic layer mesh, uses mat_id to label layers

--- testing/2Dhillslope/mesh/mesh_partition.sh
+++ testing/2Dhillslope/mesh/mesh_partition.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 #!/bin/sh
-file_in=tri_exp1_2D.exo
-file_out=4/tri_exp1_2D.par
+file_in=test_organic.exo
+file_out=4/test_organic.par
 mpiexec -n 4 $AMANZI_TPLS_DIR/bin/meshconvert --classify=1 --partition=y --partition-method=2 $file_in $file_out
